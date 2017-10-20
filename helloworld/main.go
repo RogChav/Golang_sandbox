66e21fa1f6f418007246405fd861d5c2b4c6d296
Roger Chavez <roger.chavez.03@gmail.com>
2017-10-20 15:44:30 -0700

    removing deleted file status on git

--- helloworld/main.go
+++
@@ -1,8 +0,0 @@
-package main //Need's to be named main if you want to built into an executable
-
-import "fmt" //give this package access to a standard library package in order to be linked to any of the standard library it needs to be imported. We can see more packages and descriptions at this link. https://golang.org/pkg
-
-//func is a function. Declare a function, name it and go through the process of creating the function the same way I create them in javascript
-func main() {
-	fmt.Println("Hi there!")
-}
